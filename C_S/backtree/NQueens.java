6e4703da8b07a95752f6ffafb14a1d69eb0296c6
Bibek1604 <npbibek94@gmail.com>
2024-12-22 08:10:27 +0545

    N queen problem analyzed

---
+++ C_S/backtree/NQueens.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueens {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[][] board = new char[n][n];
+        // Initialize the board with empty cells
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        solve(0, board, solutions, n);
+        return solutions;
+    }
+
+    private static void solve(int row, char[][] board, List<List<String>> solutions, int n) {
+        if (row == n) {
+            solutions.add(construct(board));
+            return;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col, board, n)) {
+                board[row][col] = 'Q'; // Place the queen
+                solve(row + 1, board, solutions, n);
+                board[row][col] = '.'; // Backtrack
+            }
+        }
+    }
+
+    private static boolean isSafe(int row, int col, char[][] board, int n) {
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<String> construct(char[][] board) {
+        List<String> result = new ArrayList<>();
+        for (char[] row : board) {
+            result.add(new String(row));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        List<List<String>> solutions = solveNQueens(n);
+        System.out.println("Number of solutions for " + n + "-Queens: " + solutions.size());
+        for (List<String> solution : solutions) {
+            for (String row : solution) {
+                System.out.println(row);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ C_S/backtree/permutation.java
@@ -0,0 +1,9 @@
+public package backtree;
+
+class permutation {
+
+    public static void main(String[] args) {
+        
+    }
+    void  findpermutation(String sr)
+}
